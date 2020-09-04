15865d58a2775cc6c59850fc30ad68bfc4223e0b
Rojan Karakaya <rojankarakaya@gmail.com>
2020-09-04 14:12:28 +0200

    travis

--- ShashiRojanlab3/R/a.R
+++ ShashiRojanlab3/R/a.R
@@ -1,2 +1,10 @@
+#' multiply by 2
+#' 
+#' @param x A number.
+#' @return x times 2.
+#' @examples
+#' a(5)
+#' a(1)
+
 a <-
 function(x){return(x*2)}
--- ShashiRojanlab3/R/b.R
+++ ShashiRojanlab3/R/b.R
@@ -1,2 +1,10 @@
-a <-
+#' multiply by 3
+#' 
+#' @param x A number.
+#' @return x times 3.
+#' @examples
+#' b(5)
+#' b(1)
+
+b <-
 function(x){return(x*3)}
